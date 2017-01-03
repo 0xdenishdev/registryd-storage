9c8219a2606f279d4d01f553f0a74bc0121ef858
denish <denishorobtsov@gmail.com>
2017-01-03 10:24:15 +0300

    Initial commit.

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => getenv('DB_DSN'),
+    'username' => getenv('DB_USERNAME'),
+    'password' => getenv('DB_PASSWORD'),
+    'charset' => 'utf8',
+];
